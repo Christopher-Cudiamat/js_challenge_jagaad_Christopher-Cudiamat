1c2e190543eab6fad7dd91842118761e289b6c7f
Christopher Cudiamat <christophervcudiamat.13@gmail.com>
2021-04-20 01:33:21 +0800

    Created static Footer section

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "./App.scss";
+import Footer from "./components/Footer/Footer.component";
 import Header from "./components/Header/Header.component";
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 };
---
+++ my-app/src/components/Footer/Footer.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.style.scss";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p className="footer-sidenote">FOOTER</p>
+    </footer>
+  );
+};
+
+export default Footer;
